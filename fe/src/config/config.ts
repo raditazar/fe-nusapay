ef9d2b8ea6e76832c40bab90b04ecad426f54894
scientivan <dienmuhammad030406@gmail.com>
2025-07-01 16:27:10 +0700

    trying to fix error

--- fe/src/config/config.ts
+++ fe/src/config/config.ts
@@ -1,9 +1,9 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { arbitrumSepolia } from 'wagmi/chains'
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { arbitrumSepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: 'NusaPay',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: "NusaPay",
+  projectId: "YOUR_PROJECT_ID",
   chains: [arbitrumSepolia],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+  ssr: false, // If your dApp uses server side rendering (SSR)
 });
